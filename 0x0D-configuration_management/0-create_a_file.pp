b3e7f14a68489e1289d075324a76fbb488a49194
Tope <tope628@gmail.com>
2017-10-24 19:15:37 +0000

    puppet checks

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+#create a file using puppet
+
 file { '/tmp/holberton':
-ensure => file,
-content => "I love Puppet",
-mode => '0744',
-owner => 'www-data',
-group => 'www-data'
+ensure  => file,
+content => 'I love Puppet',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data'
 }
